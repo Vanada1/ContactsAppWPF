7d2c0e2b21f9191baf6158c8abb94f3d59319406
Vladimir Shvoev <shvoev.work@gmail.com>
2021-02-27 13:39:25 +0700

    Добавлены кортинки для главного окна

---
+++ ContactsAppUI/Controls/BirthdayControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ContactsAppUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for BirthdayControl.xaml
+    /// </summary>
+    public partial class BirthdayControl : UserControl
+    {
+        public BirthdayControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ContactsAppUI/Controls/PersonDataControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ContactsAppUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for PersonDataControl.xaml
+    /// </summary>
+    public partial class PersonDataControl : UserControl
+    {
+        public PersonDataControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
