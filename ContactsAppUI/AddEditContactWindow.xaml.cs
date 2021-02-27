8db4312a66dd6499970c864359ba314577f653bb
Vladimir Shvoev <shvoev.work@gmail.com>
2021-02-27 16:33:06 +0700

    Сделана верстка всех окон

---
+++ ContactsAppUI/AddEditContactWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ContactsAppUI
+{
+    /// <summary>
+    /// Interaction logic for AddEditContactWindow.xaml
+    /// </summary>
+    public partial class AddEditContactWindow : Window
+    {
+        public AddEditContactWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
