78dbb2a70f6f5596411c7b8935a9600942bd7021
vladimir.shvoev <vladimir.shvoev@50ohm.tech>
2023-09-28 16:49:52 +0700

    Добавлена фреймворк React

--- ContactsWebApp/Program.cs
+++ ContactsWebApp/Program.cs
@@ -2,28 +2,26 @@ using ContactsWebApp.Data;
 using ElectronNET.API;
 using ElectronNET.API.Entities;
 using Microsoft.EntityFrameworkCore;
+using React.AspNet;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseElectron(args);
 
 builder.Services.AddElectron();
+builder.Services.AddReact();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ContactsAppDbContext>(
     options =>
-        options.UseSqlServer(
-            builder.Configuration.GetConnectionString("ContactsAppDbContext")));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ContactsAppDbContext")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Home/Error");
-}
+if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Home/Error");
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -31,13 +29,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    "default",
+    "{controller=Home}/{action=Index}/{id?}");
 
-if (HybridSupport.IsElectronActive)
-{
-    CreateElectronWindow();
-}
+if (HybridSupport.IsElectronActive) CreateElectronWindow();
 
 app.Run();
 
@@ -46,17 +41,16 @@ async void CreateElectronWindow()
     // Решение https://stackoverflow.com/questions/70544430/custom-window-with-min-max-close-buttons-in-electron-net-app-with-asp-net-or-b
     var options = new BrowserWindowOptions
     {
-
         // Frame = false,
 
         WebPreferences = new WebPreferences
         {
-            //ContextIsolation = true,
+            ContextIsolation = false,
             //DevTools = true,
             //WebSecurity = false,
             //EnableRemoteModule = true,
-            //NodeIntegration = true,
-        },
+            NodeIntegration = true
+        }
     };
 
     var window = await Electron.WindowManager.CreateWindowAsync(options);
